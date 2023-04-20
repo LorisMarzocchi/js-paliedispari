7b16ef204007c667faf704cba4412d6a2a80c52f
LorisMarzocchi <ml77182a@gmail.com>
2023-04-20 19:38:48 +0200

    addtest

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -5,17 +5,54 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 const parola = prompt( ' inserisci una parola per controllora se è palindroma ');
 
+function funzioneControllo(p1) {
+
+    let parolaReverse = "";
+
+    for (let i = p1.length - 1; i >= 0; i--) { 
+
+        parolaReverse += p1[i];
+    }
+
+    if (parolaReverse === p1) {
+
+        alert('La parola è palindroma'); 
+    } 
+    else{
+
+        alert('La parola non è palindroma');
+    }    
+}
+const controllo = funzioneControllo(parola); 
+console.log(controllo);
+
+/*
 function controlloParola(p1){
+    let reverse = p1.split("").reverse().join("");
     
-    const valori = parola.split("");
+    //const valori = p1.split("");
 
-    const valoriRev = valori.reverse(); 
+    //const valoriRev = valori.reverse(); 
 
-    const ritornoStringa = valoriRev.join('');
+    //const ritornoStringa = valoriRev.join('');
 
     
-    return (parola == ritornoStringa ? alert('è Palindroma') : alert('non è Palindroma'));
+    return (reverse == p1 ? alert('La parola è palindroma') : alert('La parola non è palindroma'));
     
 }
-const controllo = controlloParola(parola); 
-console.log(controllo);
+*/
+
+/*
+let parolaInvertita = "";
+
+for (let i = 0; i < parola.length; i++) {
+  parolaInvertita = parola[i] + parolaInvertita;
+}
+
+if (parola === parolaInvertita) {
+  console.log("La parola è palindroma.");
+} else {
+  console.log("La parola non è palindroma.");
+}
+*/
+
--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -5,7 +5,7 @@
 // Dichiariamo chi ha vinto.
 
 
-debugger;
+// debugger;
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 const utentePariDispari = prompt('scrivi se secondo te la somma sarà pari o dispari: ');
 // inserisce un numero da 1 a 5.
@@ -13,16 +13,16 @@ const numUtente = parseInt(prompt('inserisci  un n da uno a 5: '));
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 function getRndIntegerPc() {
-    
+
   return Math.floor(Math.random() * 5) + 1;
 }
 // Valore utente + Valore rndPcNumber
 let somma = numUtente + getRndIntegerPc();
 
-if (somma % 2 == 0 && utentePariDispari === 'pari' || somma % 2 !== 0 && utentePariDispari === 'dispari' ) {
-  console.log('hai vinto');
+if (somma % 2 == 0 && utentePariDispari === 'pari' || somma % 2 !== 0 && utentePariDispari === 'dispari') {
+  alert('hai vinto');
 }
 else{
-  console.log('hai perso');
+  alert('hai perso');
 
 }
